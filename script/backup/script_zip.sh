f5fc5855ac3e8923464f341445bcf75ad983960f
Aleksandr Prokopovich <prokopovich.as01@gmail.com>
2023-05-17 21:58:16 +0300

    sc2

--- script/backup/script_zip.sh
+++ script/backup/script_zip.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-vm="name_vm"
-dir="/mnt/C
+vm="namevm"
+dir="/root/Linux/scripts/backup/$vm"
+data=$(date+"%Y_%m_%d")
+file1=$(ls $dir | grep /*.qcow2)
+file2=$(ls $dir | grep /*.xml)
+
+cd $dir
+    if [ -]; then
+
