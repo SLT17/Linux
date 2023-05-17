c8892698d936b95bc7742e3522aa48b194a89dc2
Aleksandr Prokopovich <prokopovich.as01@gmail.com>
2023-05-17 23:17:34 +0300

    sc3

--- script/backup/script_zip.sh
+++ script/backup/script_zip.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 
 vm="namevm"
-dir="/root/Linux/scripts/backup/$vm"
+dir="/root/Linux/script/backup/$vm"
 data=$(date+"%Y_%m_%d")
 file1=$(ls $dir | grep /*.qcow2)
 file2=$(ls $dir | grep /*.xml)
 
 cd $dir
-    if [ -]; then
+    if [ -d $dir ]; then
+        if [ -f $file1 || $file2 ]; then
+            tar -czvf $vm_"$data".tar.gz $file1 $file2
+        else
+            echo "Dir isn't exist" > text1.txt
+        fi 
+    else
+    echo 
+    fi
+    if [ -f $zipfile]; then
+    else
+    fi
+    echo | mail -s "" prok@gmail.com
+
+exit 0
+
+
 
