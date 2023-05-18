e3e71f6f6fab7c71eb430fdad6ce288aae918c1b
Aleksandr <71207718+SLT17@users.noreply.github.com>
2023-05-18 16:58:05 +0300

    sc7

--- script/backup/script_zip.sh
+++ script/backup/script_zip.sh
@@ -1,34 +1,36 @@
 #!/bin/bash
 
 vm="namevm"
-dir="/root/Linux/script/backup/$vm"
-data=$(date+"%Y_%m_%d")
+dir="way_dir/$vm"
+data=$(date "+%Y_%m_%d")
 file1=$(ls $dir | grep /*.qcow2)
 file2=$(ls $dir | grep /*.xml)
-logfile=""$vm"_"$data".txt"
-cd $dir
-    if [ -d "$dir" ]; then
-        if [ -f $file1 ] || [ $file2 ]; then
-            tar -czvf $vm_"$data".tar.gz $file1 $file2
-				process_id0=$!
-				wait process_id0
-				rm -f $file1 $file2
-				zipfile=$(ls $dir | grep /*.tar.gz)
-				if [ -f $zipfile]; then 
-				chown backup0:backup0 $zipfile
-				else
-				echo "Error file owner" > $dir/$logfile
-				fi
+logfile=("$vm"_"$data".txt)
+
+if [ -d "$dir" ]; then
+        cd $dir
+        if [ -f "$dir/$file1" ] || [ -f "$dir/$file2" ]; then
+            tar -czvf "$vm"_"$data".tar.gz $file1 $file2
+                                process_id0=$!
+                                wait $process_id0
+                                rm -f $file1 $file2
+                                zipfile=$(ls $dir | grep /*.tar.gz)
+                                if [ -f $zipfile ]; then
+                                    chown backup0:backup0 $zipfile
+                                else
+                                    echo "Error file owner" > $dir/$logfile
+                        fi
+                if [ -f "$dir/$logfile" ]; then
+				# Предварительно настроить отправку писем с сервера (mail+ssmtp)
+                    cat $dir/$logfile | mail -s "Zip $vm $data" mail@gmail.com
+                    process_id0=$!
+                    wait $process_id0
+                    rm -f $dir/$logfile
+                fi
         else
             echo "Files isn't exist" > $dir/$logfile
-        fi 
+        fi
     else
-	echo "Dir isn't exist" > $dir/$logfile
-    fi
-	if [ -f $dir/]
-    cat $dir/$logfile | mail -s $logfile prok@gmail.com
-
+        echo "Dir isn't exist" > $dir/$logfile
+fi
 exit 0
-
-
-
